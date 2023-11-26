d59e2514714aef44a0f6ccc78da8a68b5e2775b0
Claderoki <49477117+claderoki@users.noreply.github.com>
2023-11-26 17:16:54 +0100

    no message

--- src/main/java/com/challenges/AddTwoNumbers.java
+++ src/main/java/com/challenges/AddTwoNumbers.java
@@ -2,50 +2,114 @@ package com.challenges;
 
 import com.challenges.base.CodeChallenge;
 import com.challenges.base.InputOutput;
-import org.w3c.dom.NodeList;
 
-import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
+import java.util.Stack;
 
 /**
  * <a href="https://leetcode.com/problems/add-two-numbers/">Link to challenge</a>
  */
 public class AddTwoNumbers extends CodeChallenge<AddTwoNumbers.AddTwoNumbersInput, AddTwoNumbers.ListNode> {
     public record AddTwoNumbersInput(ListNode l1, ListNode l2) {}
+
+    @Override
+    public boolean isEqual(ListNode output1, ListNode output2) {
+        return output1.format().equals(output2.format());
+    }
+
+    @Override
+    public String format(ListNode output) {
+        return output.format();
+    }
+
     public static class ListNode {
         int val;
         ListNode next;
-        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+
+        public String format() {
+            StringBuilder stringBuilder = new StringBuilder("[");
+            ListNode node = this;
+            while (node != null) {
+                stringBuilder.append(node.val);
+                node = node.next;
+                if (node != null) {
+                    stringBuilder.append(",");
+                }
+            }
+            return stringBuilder.append("]").toString();
+        }
     }
 
-    private ListNode into(int numbers) {
-        var builder = String.valueOf(numbers);
+    public Stack<Integer> toStack(ListNode node) {
+        Stack<Integer> numbers = new Stack<>();
+        while (node != null) {
+            numbers.insertElementAt(node.val, 0);
+            node = node.next;
+        }
+        return numbers;
+    }
+
+    private ListNode into(String numbers) {
         ListNode child = null;
-        for (int i = 0; i < builder.length(); i++) {
-            child = new ListNode(Integer.parseInt(String.valueOf(builder.charAt(i))), child);
+        for (char number: numbers.toCharArray()) {
+            child = new ListNode(Integer.parseInt(String.valueOf(number)), child);
         }
         return child;
     }
 
-    private int collect(ListNode node) {
-        StringBuilder digits = new StringBuilder();
-        do {
-            digits.insert(0, String.valueOf(node.val));
+    private ListNode reverse(ListNode node) {
+        ListNode child = null;
+        while (node != null) {
+            child = new ListNode(node.val, child);
             node = node.next;
-        } while (node != null);
-        return Integer.parseInt(digits.toString());
+        }
+        return child;
+    }
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        Stack<Integer> first = toStack(l1);
+        Stack<Integer> second = toStack(l2);
+
+        int remainder = 0;
+        ListNode child = null;
+        while (!first.isEmpty() || !second.isEmpty()) {
+            var lastOfFirst = first.isEmpty() ? 0 : first.pop();
+            var lastOfSecond = second.isEmpty() ? 0 : second.pop();
+
+            int sum = lastOfFirst + lastOfSecond + remainder;
+            if (sum > 9) {
+                remainder = sum / 10;
+                sum = sum % 10;
+            } else {
+                remainder = 0;
+            }
+            child = new ListNode(sum, child);
+        }
+
+        if (remainder > 0) {
+            child = new ListNode(remainder, child);
+        }
+        return reverse(child);
     }
 
     public ListNode test(AddTwoNumbersInput input) {
-        return into(collect(input.l1) + collect(input.l2));
+        return addTwoNumbers(input.l1, input.l2);
     }
+
+    private AddTwoNumbersInput input(String l1, String l2) {
+        return new AddTwoNumbersInput(into(l1), into(l2));
+    }
+
     @Override
     protected List<InputOutput<AddTwoNumbersInput, ListNode>> getInputAndDesiredOutputs() {
         return List.of(
-//            new InputOutput<>(into(807), into(702))
+            new InputOutput<>(input("1000000000000000000000000000001", "564"), into("1000000000000000000000000000565")),
+            new InputOutput<>(input("243", "564"), into("807")),
+            new InputOutput<>(input("9999999", "9999"), into("10009998"))
         );
     }
-
-
 }
--- src/main/java/com/challenges/base/CodeChallenge.java
+++ src/main/java/com/challenges/base/CodeChallenge.java
@@ -10,19 +10,29 @@ public abstract class CodeChallenge<I, O> {
         return output1.equals(output2);
     }
 
-    public void testAll() {
-        int i = 0;
-        for(var inputOutput: getInputAndDesiredOutputs()) {
-            var output = test(inputOutput.input);
-            if (!isEqual(output, inputOutput.output)) {
-                throw new RuntimeException(output + " != " + inputOutput.output);
-            }
-            i++;
-        }
-        System.out.printf("All test cases succeeded (%s/%s)", i, i);
+    public String format(O output) {
+        return output.toString();
     }
 
-    protected static int[] array(int... values) {
+    public void testAll() {
+        int i = 0;
+        long totalMillis = 0;
+        for(var inputOutput: getInputAndDesiredOutputs()) {
+            long startTime = System.currentTimeMillis();
+            var output = test(inputOutput.input);
+            long endTime = System.currentTimeMillis();
+            if (!isEqual(output, inputOutput.output)) {
+                throw new RuntimeException(format(output) + " != " + format(inputOutput.output));
+            }
+            long millis = endTime - startTime;
+            System.out.printf("Test cases succeeded (%s/%s) in %s ms\n", i, i, millis);
+            totalMillis += millis;
+            i++;
+        }
+        System.out.printf("All test cases succeeded (%s/%s) in %s", i, i, totalMillis);
+    }
+
+    protected int[] array(int... values) {
         return values;
     }
 }
