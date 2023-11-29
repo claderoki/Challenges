b88f35ad2727895f99283f9f269778a0c66e55c7
Claderoki <49477117+claderoki@users.noreply.github.com>
2023-11-29 22:46:45 +0100

    no message

--- src/main/java/com/challenges/MedianOfTwoSortedArrays.java
+++ src/main/java/com/challenges/MedianOfTwoSortedArrays.java
@@ -6,6 +6,21 @@ import com.challenges.base.InputOutput;
 import java.util.List;
 
 /**
+ * Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
+ *
+ * The overall run time complexity should be O(log (m+n)).
+ *
+ * Example 1:
+ *
+ * Input: nums1 = [1,3], nums2 = [2]
+ * Output: 2.00000
+ * Explanation: merged array = [1,2,3] and median is 2.
+ *
+ * Example 2:
+ *
+ * Input: nums1 = [1,2], nums2 = [3,4]
+ * Output: 2.50000
+ * Explanation: merged array = [1,2,3,4] and median is (2 + 3) / 2 = 2.5.
  * <a href="https://leetcode.com/problems/median-of-two-sorted-arrays/">Link to challenge</a>
  */
 public class MedianOfTwoSortedArrays extends CodeChallenge<MedianOfTwoSortedArrays.Input, Double> {
@@ -19,7 +34,8 @@ public class MedianOfTwoSortedArrays extends CodeChallenge<MedianOfTwoSortedArra
     @Override
     protected List<InputOutput<Input, Double>> getInputAndDesiredOutputs() {
         return List.of(
-            io(new Input(array(1,3), array(2)), 2.00000D)
+            io(new Input(array(1,3), array(2)), 2.00000D),
+            io(new Input(array(1,2), array(3,4)), 2.50000D)
         );
     }
 
