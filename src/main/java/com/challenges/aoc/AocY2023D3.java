c256118c93f18b65b2d4c9620f482bd3a5a2eb9b
Claderoki <49477117+claderoki@users.noreply.github.com>
2023-12-03 14:45:53 +0100

    no message

--- src/main/java/com/challenges/aoc/AocY2023D3.java
+++ src/main/java/com/challenges/aoc/AocY2023D3.java
@@ -3,7 +3,7 @@ package com.challenges.aoc;
 import com.challenges.base.AdventOfCode;
 
 import java.util.*;
-import java.util.function.Function;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 /** Completed
@@ -14,138 +14,133 @@ public class AocY2023D3 extends AdventOfCode<Integer> {
         super(2023, 3);
     }
 
-    private final List<Character> symbols = List.of(
-        '*',
-        '#',
-        '+',
-        '$'
-    );
-
     record Digit(int line, int index, Integer value) {}
-    record Indexes(int start, int end) {}
 
-    private Optional<Indexes> findDigits(String input, int center) {
-        boolean any = false;
-        int end = center;
-        int current = input.charAt(end);
-        while (end < input.length()-1 && Character.isDigit(current)) {
-            any = true;
-            end++;
-            current = input.charAt(end);
-        }
-
-        int start = center;
-        current = input.charAt(start);
-        while (start > 0 && Character.isDigit(current)) {
-            any = true;
-            start--;
-            current = input.charAt(start);
-        }
-        if (!any) {
+    private Optional<Digit> findDigits(List<String> lines, int lineIndex, int center) {
+        StringBuilder digits = new StringBuilder();
+        if (lineIndex < 0 || lineIndex >= lines.size()) {
             return Optional.empty();
         }
+        String line = lines.get(lineIndex);
+        if (center > 0 && center < line.length() && !Character.isDigit(line.charAt(center))) {
+            return Optional.empty();
+        }
+        center = Math.min(Math.max(center, 0), line.length()-1);
 
-        return Optional.of(new Indexes(start, end));
+        int start = center;
+        for(int i = center; i <= line.length()-1; i++) {
+            char character = line.charAt(i);
+            if (Character.isDigit(character)) {
+                digits.append(character);
+            } else {
+                break;
+            }
+        }
+        for(int i = center-1; i >= 0; i--) {
+            char character = line.charAt(i);
+            if (Character.isDigit(character)) {
+                digits.insert(0, character);
+                start = i;
+            } else {
+                break;
+            }
+        }
+
+        if (digits.isEmpty()) {
+            return Optional.empty();
+        }
+        return Optional.of(new Digit(lineIndex, start, Integer.parseInt(digits.toString())));
     }
 
     public Integer part1(String input) {
-        Set<Digit> partNumbers = new HashSet<>();
+        List<Digit> partNumbers = new ArrayList<>();
+        List<String> lines = input.lines().toList();
+        for (int j = 0; j < lines.size(); j++) {
+            System.out.println("Line " + (j+1));
+            final String line = lines.get(j);
+            for (int i = 0; i < line.length(); i++) {
+                char character = line.charAt(i);
+                boolean isSymbol = character != '.' && character != '\n' && !Character.isDigit(character);
+                if (isSymbol) {
+                    int LINE = j;
+                    int DOWN = LINE+1;
+                    int UP = LINE-1;
+                    int HERE = i;
+                    int RIGHT = HERE+1;
+                    int LEFT = HERE-1;
 
+                    var found = Stream.of(
+                            findDigits(lines, UP, HERE),
+                            findDigits(lines, UP, LEFT),
+                            findDigits(lines, UP, RIGHT),
+                            findDigits(lines, DOWN, HERE),
+                            findDigits(lines, DOWN, LEFT),
+                            findDigits(lines, DOWN, RIGHT),
+                            findDigits(lines, LINE, LEFT),
+                            findDigits(lines, LINE, RIGHT)
+                        )
+                        .filter(Optional::isPresent)
+                        .map(Optional::get)
+                        .collect(Collectors.toSet());
+
+                    var readable = found.stream().map(c -> c.value.toString()).collect(Collectors.joining(", "));
+                    System.out.printf("numbers near %s found: %s%n", character, readable);
+
+                    partNumbers.addAll(found);
+                }
+            }
+        }
+        return partNumbers.stream().mapToInt(c -> c.value).sum();
+    }
+
+    public Integer part2(String input) {
+        long total = 0;
         List<String> lines = input.lines().toList();
         for (int j = 0; j < lines.size(); j++) {
             final String line = lines.get(j);
             for (int i = 0; i < line.length(); i++) {
                 char character = line.charAt(i);
-                if (symbols.contains(character)) {
-//                    Function<> seek = () -> {
-//                        return null;
-//                    };
-//
-//                    var a = Stream.of(
-//                    );
+                boolean isSymbol = character != '.' && character != '\n' && !Character.isDigit(character);
+                if (isSymbol) {
+                    int LINE = j;
+                    int DOWN = LINE+1;
+                    int UP = LINE-1;
+                    int HERE = i;
+                    int RIGHT = HERE+1;
+                    int LEFT = HERE-1;
 
+                    var found = Stream.of(
+                            findDigits(lines, UP, HERE),
+                            findDigits(lines, UP, LEFT),
+                            findDigits(lines, UP, RIGHT),
+                            findDigits(lines, DOWN, HERE),
+                            findDigits(lines, DOWN, LEFT),
+                            findDigits(lines, DOWN, RIGHT),
+                            findDigits(lines, LINE, LEFT),
+                            findDigits(lines, LINE, RIGHT)
+                        )
+                        .filter(Optional::isPresent)
+                        .map(Optional::get)
+                        .distinct()
+                        .map(Digit::value)
+                        .toList();
 
-//                    int sameIndex = j * lineHeight + current;
-//                    int topIndex = (j-1) * lineHeight + current;
-//                    int bottomIndex = (j+1) * lineHeight + current;
-
-//                System.out.println("[%s][%s][%s]".formatted(topLeft, top, topRight));
-//                System.out.println("[%s][%s][%s]".formatted(left, character, right));
-//                System.out.println("[%s][%s][%s]".formatted(bottomLeft, bottom, bottomRight));
-
-//                    var top = findDigits(lines.get(i-1), topIndex);
-//                    var topLeft = findDigits(lines.get(i-1), topIndex - 1);
-//                    var topRight = findDigits(line, topIndex + 1);
-//                    var bottom = findDigits(line, bottomIndex);
-//                    var bottomLeft = findDigits(line, bottomIndex - 1);
-//                    var bottomRight = findDigits(line, bottomIndex + 1);
-//                    var left = findDigits(line, sameIndex - 1);
-//                    var right = findDigits(line, sameIndex + 1);
-
-//                    partNumbers.addAll(Stream.of(top, topLeft, topRight, bottom, bottomLeft, bottomRight, left, right)
-//                        .filter(Optional::isPresent)
-//                        .map(Optional::get)
-//                        .toList());
+                    if (character == '*') {
+                        if (found.size() == 2) {
+                            total += ((long) found.get(0) * found.get(1));
+                            System.out.printf("numbers near %s found: %s * %s%n", character, found.get(0), found.get(1));
+                        }
+                    }
                 }
             }
         }
-
-
-        Integer lineHeight = null;
-        int j = 0;
-        for (int i = 0; i < input.length(); i++) {
-            int current = i-(j*(lineHeight != null ? lineHeight : 0));
-            char character = input.charAt(i);
-            if (character == '\n') {
-                if (lineHeight == null) {
-                    lineHeight = i+1;
-                }
-                j++;
-            } else if (symbols.contains(character)) {
-                int sameIndex = j * lineHeight + current;
-                int topIndex = (j-1) * lineHeight + current;
-                int bottomIndex = (j+1) * lineHeight + current;
-
-//                char top = input.charAt(topIndex);
-//                char topLeft = input.charAt(topIndex - 1);
-//                char topRight = input.charAt(topIndex + 1);
-//                char bottom = input.charAt(bottomIndex);
-//                char bottomLeft = input.charAt(bottomIndex - 1);
-//                char bottomRight = input.charAt(bottomIndex + 1);
-//                char left = input.charAt(sameIndex - 1);
-//                char right = input.charAt(sameIndex + 1);
-
-//                System.out.println("[%s][%s][%s]".formatted(topLeft, top, topRight));
-//                System.out.println("[%s][%s][%s]".formatted(left, character, right));
-//                System.out.println("[%s][%s][%s]".formatted(bottomLeft, bottom, bottomRight));
-
-                var top = findDigits(input, lineHeight, topIndex);
-                var topLeft = findDigits(input, lineHeight, topIndex - 1);
-                var topRight = findDigits(input, lineHeight, topIndex + 1);
-                var bottom = findDigits(input, lineHeight, bottomIndex);
-                var bottomLeft = findDigits(input, lineHeight, bottomIndex - 1);
-                var bottomRight = findDigits(input, lineHeight, bottomIndex + 1);
-                var left = findDigits(input, lineHeight, sameIndex - 1);
-                var right = findDigits(input, lineHeight, sameIndex + 1);
-
-                partNumbers.addAll(Stream.of(top, topLeft, topRight, bottom, bottomLeft, bottomRight, left, right)
-                    .filter(Optional::isPresent)
-                    .map(Optional::get)
-                    .toList());
-            }
-        }
-
-        return partNumbers.stream().mapToInt(c -> c.value).sum();
-    }
-
-
-
-    public Integer part2(String input) {
-        return null;
+        return Math.toIntExact(total);
     }
 
     public void run() {
-        System.out.println("Part 1 result => " + part1(input('e')));
-//        System.out.println("Part 2 result => " + part2(input()));
+        // 533784 == correct
+        System.out.println("Part 1 result => " + part1(input()));
+//        78826761 == correct
+        System.out.println("Part 2 result => " + part2(input()));
     }
 }
